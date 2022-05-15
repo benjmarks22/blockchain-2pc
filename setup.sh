49bb442d094ba38cee3c6d2ba27eba16c9128e9e
Benjamin Marks <benjmarks22@gmail.com>
2022-05-15 14:19:30 -0400

    Add sqlite3 dep for blockchain.
    Also add Dockerfile for reproducibility.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Docker doesn't have sudo and everything is run as root there.
+if ! [ -z $IS_DOCKER ]; then
+    SUDO=''
+else
+    SUDO='sudo'
+fi
+
 function init_binary_dir () {
     BINARY_DIR="$1"
     mkdir -p "${BINARY_DIR}"
@@ -11,14 +18,14 @@ function install () {
     URL="$3"
     if ! [ -x "$(command -v ${BINARY_NAME})" ]; then
         curl --location --fail "${URL}" --output "/tmp/${BINARY_NAME}" || exit 1
-        sudo mv "/tmp/${BINARY_NAME}" "${BINARY_DIR}/${BINARY_NAME}"
+        $SUDO mv "/tmp/${BINARY_NAME}" "${BINARY_DIR}/${BINARY_NAME}"
         chmod +x "${BINARY_DIR}/${BINARY_NAME}"
     fi
 }
 
 BINARY_DIR="/usr/local/bin"
-sudo apt-get update || exit 2
-sudo apt-get install -y clang-format clang-tidy curl git gcc g++ python3 || exit 3
+$SUDO apt-get update || exit 2
+$SUDO apt-get install -y clang-format clang-tidy curl git gcc g++ python3 python3-pip || exit 3
 install ${BINARY_DIR} bazel "https://github.com/bazelbuild/bazelisk/releases/download/v1.11.0/bazelisk-linux-amd64"
 install ${BINARY_DIR} buildifier "https://github.com/bazelbuild/buildtools/releases/download/5.1.0/buildifier-linux-amd64"
 python3 -m pip install pre-commit
