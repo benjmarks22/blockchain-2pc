9c5e35fa5356225a0685aa35bbac4ace6cb7dd96
Heron Yang <heron.yang.tw@gmail.com>
2022-05-29 21:37:28 +0000

    Add startVoting implementation in adapter server.

--- src/blockchain/client/adapter_server.js
+++ src/blockchain/client/adapter_server.js
@@ -1,8 +1,8 @@
 const args = process.argv.slice(2);
 
-const blockchain_uri = 'ws://localhost:' + args[0];
-const contract_addr = args[1];
-const shared_account = args[2];
+const blockchainUri = 'ws://localhost:' + args[0];
+const contractAddr = args[1];
+const sharedAccount = args[2];
 
 const PROTO_PATH = __dirname + '/../proto/two_phase_commit_adapter.proto';
 
@@ -15,25 +15,50 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   defaults: true,
   oneofs: true
 });
-var two_phase_commit_adapter_proto =
+var twoPhaseCommitAdapterProto =
     grpc.loadPackageDefinition(packageDefinition).blockchain;
 
 var TwoPhaseCommitClient = require('./contract_client.js');
 var contractClient = new TwoPhaseCommitClient(
-    '../build/contracts/TwoPhaseCommit.json', blockchain_uri, contract_addr);
+    '../build/contracts/TwoPhaseCommit.json', blockchainUri, contractAddr);
+
+function startVoting(call, callback) {
+  console.log('Received: startVoting');
+  console.log(call.request);
+  contractClient.startVoting(
+      sharedAccount, call.request.transaction_id, call.request.cohorts,
+      call.request.timeout_time.seconds, () => {
+        console.log('Done: startVoting');
+        callback(null, {});
+      });
+}
+
+function vote(call, callback) {
+  console.log('Received: vote');
+  console.log('Done: vote');
+}
+
+function getVotingDecision(call, callback) {
+  console.log('Received: getVotingDecision');
+  console.log('Done: getVotingDecision');
+}
 
 function getHeartBeat(call, callback) {
+  console.log('Received: getHeartBeat');
   contractClient.getHeartBeat((result) => {
-    console.log('Processed: getHeartBeat');
     callback(null, {is_ok: result});
+    console.log('Done: getHeartBeat');
   });
 }
 
 function main() {
   var server = new grpc.Server();
-  server.addService(
-      two_phase_commit_adapter_proto.TwoPhaseCommitAdapter.service,
-      {getHeartBeat: getHeartBeat});
+  server.addService(twoPhaseCommitAdapterProto.TwoPhaseCommitAdapter.service, {
+    startVoting: startVoting,
+    vote: vote,
+    getVotingDecision: getVotingDecision,
+    getHeartBeat: getHeartBeat
+  });
   server.bindAsync(
       '0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
         server.start();
--- src/blockchain/client/adapter_server_test.js
+++ src/blockchain/client/adapter_server_test.js
@@ -9,15 +9,33 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   defaults: true,
   oneofs: true
 });
-var two_phase_commit_adapter_proto =
+var twoPhaseCommitAdapterProto =
     grpc.loadPackageDefinition(packageDefinition).blockchain;
 
 function main() {
-  var client = new two_phase_commit_adapter_proto.TwoPhaseCommitAdapter(
+  var adapterClient = new twoPhaseCommitAdapterProto.TwoPhaseCommitAdapter(
       'localhost:50051', grpc.credentials.createInsecure());
-  client.getHeartBeat({}, function(err, response) {
+  /*
+  adapterClient.getHeartBeat({}, function(err, response) {
     console.log('Is Ok:', response.is_ok);
   });
+  */
+  // Set timeout time as one hour later.
+  const transaction_id =
+      new Date().toISOString();     // use current time as transaction id.
+  const timeout_time = 1672560000;  // unix_time: 1/1/2032 00:00PM
+  adapterClient.startVoting(
+      {
+        transaction_id: transaction_id,
+        timeout_time: {seconds: timeout_time, nanos: 0},
+        cohorts: 3
+      },
+      function(err, response) {
+        if (err) {
+          console.log('StartVoting Error', err);
+        }
+        console.log('StartVoting Response', response)
+      });
 }
 
 main();
